3daa0433bb143a208abb48edc62532191d59bc6a
Nashwan Andayuri <nashwanhafidandayuri@mail.ugm.ac.id>
2026-06-05 15:51:42 +0700

    replay button

--- scripts/win_screen.gd
+++ scripts/win_screen.gd
@@ -3,7 +3,7 @@ extends Control
 
 
 func _on_return_button_pressed() -> void:
-	get_tree().reload_current_scene() # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/test_level.tscn") # Replace with function body.
 
 
 func _on_quit_button_pressed() -> void:
